0549f049ee22a9db2db79bb04e55dc64ce667a69
Hunter Carlson <carlson.hunter@gmail.com>
2016-09-19 14:03:33 -0700

    Added on/off animations to the logo and connection led

--- FadeCandyGui/MainWindow.xaml.cs
+++ FadeCandyGui/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Effects;
 using OpenPixelControl;
 
 namespace FadeCandyGui
@@ -14,15 +18,25 @@ namespace FadeCandyGui
         private int _port;
         private string _server;
 
+        private bool _prevButtonState = false;
 
-        const double onOpacity = 88;
-        const double offOpacity = 88;
+
+        private const double LightToggleAnimLength = 1.8;
+
+        private const double LedOnOpacity = 0.88;
+        private const double LedOffOpacity = 0.35;
+        private const double LogoOnOpacity = 1.00;
+        private const double LogoOffOpacity = 0.40;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            ConnectionStatusLedImage.Opacity = offOpacity;
+            // set lighting effects to off
+            ConnectionStatusLedImage.Opacity = LedOffOpacity;
+            LogoImage.Opacity = LogoOffOpacity;
+            LogoImageBlurLayer.Opacity = LogoOffOpacity;
+
             //_opcClient = new OpcClient();
         }
 
@@ -43,19 +57,71 @@ namespace FadeCandyGui
             _offDuration = e.NewValue;
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
         private void ConnectButton_Click(object sender, RoutedEventArgs e)
         {
-            // 0 - 100
-            // 88 = on
-            // 35 = off
+            // if prev was on
+            if (_prevButtonState)
+            {
+                // turn off
 
+                //clear previous effects
+                ConnectionStatusLedImage.ClearValue(EffectProperty);
+                LogoImageBlurLayer.ClearValue(EffectProperty);
 
-            ConnectionStatusLedImage.Opacity = onOpacity;
+                //create new effects
+                var ledBlurEffect = new BlurEffect();
+                var logoBlurEffect = new BlurEffect();
+
+                //assign effects
+                ConnectionStatusLedImage.Effect = ledBlurEffect;
+                LogoImageBlurLayer.Effect = logoBlurEffect;
+
+                //create animations
+                var ledBlurRadiusAnimation = new DoubleAnimation(5, 0, TimeSpan.FromSeconds(LightToggleAnimLength));
+                var logoImageBlurRadiusAnimation = new DoubleAnimation(20, 0, TimeSpan.FromSeconds(LightToggleAnimLength));
+                var ledOpacityAnimation = new DoubleAnimation(LedOnOpacity, LedOffOpacity, TimeSpan.FromSeconds(LightToggleAnimLength));
+                var logoOpacityAnimation = new DoubleAnimation(LogoOnOpacity, LogoOffOpacity, TimeSpan.FromSeconds(LightToggleAnimLength));
+
+                //start animations
+                ledBlurEffect.BeginAnimation(BlurEffect.RadiusProperty, ledBlurRadiusAnimation);
+                logoBlurEffect.BeginAnimation(BlurEffect.RadiusProperty, logoImageBlurRadiusAnimation);
+                ConnectionStatusLedImage.BeginAnimation(OpacityProperty, ledOpacityAnimation);
+                LogoImageBlurLayer.BeginAnimation(OpacityProperty, logoOpacityAnimation);
+                LogoImage.BeginAnimation(OpacityProperty, logoOpacityAnimation);
+            }
+            else
+            {
+                //else turn on
+
+                //clear previous effects
+                ConnectionStatusLedImage.ClearValue(EffectProperty);
+                LogoImageBlurLayer.ClearValue(EffectProperty);
+
+                //create new effects
+                var ledBlurEffect = new BlurEffect();
+                var logoBlurEffect = new BlurEffect();
+
+                //assign effects
+                ConnectionStatusLedImage.Effect = ledBlurEffect;
+                LogoImageBlurLayer.Effect = logoBlurEffect;
+
+                //create animations
+                var ledBlurRadiusAnimation = new DoubleAnimation(0, 5, TimeSpan.FromSeconds(LightToggleAnimLength));
+                var logoImageBlurRadiusAnimation = new DoubleAnimation(0, 20, TimeSpan.FromSeconds(LightToggleAnimLength));
+                var ledOpacityAnimation = new DoubleAnimation(LedOffOpacity, LedOnOpacity, TimeSpan.FromSeconds(LightToggleAnimLength));
+                var logoOpacityAnimation = new DoubleAnimation(LogoOffOpacity, LogoOnOpacity, TimeSpan.FromSeconds(LightToggleAnimLength));
+
+                //start animations
+                ledBlurEffect.BeginAnimation(BlurEffect.RadiusProperty, ledBlurRadiusAnimation);
+                logoBlurEffect.BeginAnimation(BlurEffect.RadiusProperty, logoImageBlurRadiusAnimation);
+                ConnectionStatusLedImage.BeginAnimation(OpacityProperty, ledOpacityAnimation);
+                LogoImageBlurLayer.BeginAnimation(OpacityProperty, logoOpacityAnimation);
+                LogoImage.BeginAnimation(OpacityProperty, logoOpacityAnimation);
+            }
+
+            //toggle button state
+            _prevButtonState = !_prevButtonState;
         }
+
     }
 }
